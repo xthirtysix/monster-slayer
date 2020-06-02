a05a65996f3483e3606d370de4192b08896c67de
Aleksei Bezdetko <ggalliosha@gmail.com>
2020-06-02 17:41:48 +0300

    Add action-bar that starts game, and controles battle

--- src/App.vue
+++ src/App.vue
@@ -2,28 +2,34 @@
   <div id="app" class="container">
     <h1 class="main-header">{{ name }}</h1>
     <Rivals/>
+    <Actionbar
+      @start-end-game="isRunning = !isRunning"
+      :running="isRunning"/>
   </div>
 </template>
 
 <script>
 import Rivals from './components/Rivals.vue';
+import Actionbar from './components/Actionbar.vue';
 
 export default {
   name: 'Monster Slayer',
   data() {
     return {
       name: 'Monster Slayer',
+      isRunning: false,
     };
   },
   components: {
     Rivals,
+    Actionbar,
   },
 };
 </script>
 
 
 <style lang="scss">
-  @import '@/scss/_mixins.scss';
+  @import './scss/_mixins.scss';
 
   #app {
     @include border;
--- src/components/Actionbar.vue
+++ src/components/Actionbar.vue
@@ -1,10 +1,93 @@
 <template>
-  <section class="action-bar">
+  <section class="action-bar" v-if="!running">
+    <button
+      class="action-bar__btn action-bar__btn--start"
+      type="button"
+      @click="$emit('start-end-game')">
+      Start
+    </button>
+  </section>
+  <section class="action-bar" v-else>
+    <button
+      class="action-bar__btn action-bar__btn--attack"
+      type="button">
+      Attack
+    </button>
+    <button
+      class="action-bar__btn action-bar__btn--special"
+      type="button">
+      Special attack
+    </button>
+    <button
+      class="action-bar__btn action-bar__btn--heal"
+      type="button">
+      Heal
+    </button>
+    <button
+      class="action-bar__btn action-bar__btn--run"
+      type="button"
+      @click="$emit('start-end-game')">
+      Run!
+    </button>
   </section>
 </template>
 
 <script>
+export default {
+  props: {
+    running: Boolean,
+  },
+  data() {
+    return {
+    };
+  },
+};
 </script>
 
-<style scoped type="scss">
+<style type="scss">
+@import '../scss/_variables.scss';
+.action-bar {
+  display: grid;
+  grid-template-columns: max-content max-content max-content max-content;
+  grid-column-gap: 1rem;
+  justify-content: center;
+  padding: 2rem 0 1rem;
+}
+
+.action-bar__btn {
+  padding: 1rem 2rem;
+  text-transform: uppercase;
+  color: var(--white);
+  background-color: transparent;
+  border: none;
+  border-radius: 5px;
+  outline: none;
+  transition: 0.3s ease-in-out;
+}
+
+.action-bar__btn:hover {
+  opacity: 0.7;
+}
+
+.action-bar__btn--start {
+  background-color: var(--blue);
+}
+
+.action-bar__btn--attack {
+    background-color: var(--red);
+  }
+
+.action-bar__btn--special {
+    background-color: var(--red);
+  }
+
+.action-bar__btn--heal {
+  background-color: var(--green);
+}
+
+.action-bar__btn--run {
+  color: var(--black);
+  background-color: var(--whie);
+  border: 1px solid var(--black);
+}
 </style>
